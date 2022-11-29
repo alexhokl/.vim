2bd15fe67b94124453cbf3fe8ffca57b09b74fe2
alexhokl <alexhokl@gmail.com>
2022-11-29 14:21:07 +0800

    Added configuration for file type terraform

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -8,3 +8,11 @@ vim.api.nvim_create_autocmd({"BufEnter"}, {
 
 vim.api.nvim_buf_set_keymap(0, "n", "<C-B>", "<cmd>!make install<CR>", { noremap = true })
 
+-- local format = vim.lsp.buf.formatting_sync({
+-- 	filter = function(client) return client.name ~= "gopls" end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({"BufWritePre"}, {
+--   pattern = {"*.go"},
+--   callback = vim.lsp.buf.formatting_sync,
+-- })
