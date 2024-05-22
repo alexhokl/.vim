4d991bdf9389ba884e756086755742d9536c527e
alexhokl <alexhokl@gmail.com>
2024-05-22 20:04:05 +0800

    Updated configuration

--- plugin/db.lua
+++ plugin/db.lua
@@ -1,2 +1,11 @@
+local cmp = require 'cmp'
+
+cmp.setup.filetype({ "sql" }, {
+  sources = {
+	{ name = 'vim-dadbod-completion' },
+	{ name = 'buffer' },
+  },
+})
+
 vim.g.db_ui_use_nerd_fonts = 1
 
