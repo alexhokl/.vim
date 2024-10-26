5661112d7d876d4960011d4459fc4007c3272888
alexhokl <alexhokl@gmail.com>
2024-10-27 06:42:14 +0800

    Updated configuration

--- plugin/db.lua
+++ plugin/db.lua
@@ -1,5 +1,10 @@
 local cmp = require 'cmp'
 
+local opts = {
+  silent = true,
+  noremap = true,
+}
+
 cmp.setup.filetype({ "sql" }, {
   sources = {
 	{ name = 'vim-dadbod-completion' },
@@ -8,4 +13,19 @@ cmp.setup.filetype({ "sql" }, {
 })
 
 vim.g.db_ui_use_nerd_fonts = 1
+
+vim.g.db_ui_table_helpers = {
+	postgresql = {
+		Count = 'SELECT COUNT(*) FROM "{table}"',
+	},
+	sqlserver = {
+		Count = 'SELECT COUNT(*) FROM "{table}"',
+	},
+	sqlite = {
+		Count = 'SELECT COUNT(*) FROM "{table}"',
+	},
+}
+
+vim.keymap.set("n", "<leader>dbe", ':e ~/.local/share/db_ui/connections.json<CR>', opts)
+vim.keymap.set("n", "<leader>dbc", ':DBUIToggle<CR>', opts)
 
--- plugin/todo.lua
+++
@@ -1,4 +0,0 @@
-require("todo-comments").setup();
-
-vim.keymap.set("n", "<leader>todo", ':TodoTelescope<CR>', { noremap = true, silent = true })
-
