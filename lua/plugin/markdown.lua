f18884067b01412d24b44bb3cdf4ad628bbcf09b
alexhokl <alexhokl@gmail.com>
2025-05-18 20:05:18 +0800

    Addded nvim-md-toc

--- lua/plugin/markdown.lua
+++ lua/plugin/markdown.lua
@@ -19,4 +19,27 @@ return {
     "plasticboy/vim-markdown",
   },
 
+  {
+    "alexhokl/nvim-md-toc",
+    opts = {
+      keymap = "<leader>rm",
+    },
+    config = function()
+      local plugin = require("nvim-md-toc")
+      plugin.setup({
+        keymap = "<leader>rm",
+      })
+
+      -- Utility function for mapping keys
+      local map = function(mode, keys, command, desc)
+        vim.keymap.set(mode, keys, command, { noremap = true, silent = true, desc = desc })
+      end
+
+      local level_4 = function()
+        plugin.refresh_toc(4)
+      end
+      map("n", "<leader>rrm", level_4, "Refresh table of contents")
+    end
+  },
+
 }
