d00a2f1186c898e1c2302331ffb0d8222a67e5c6
alexhokl <alexhokl@gmail.com>
2025-07-20 18:01:41 +0800

    Fixed configuration of nvim-ts-autotag

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -76,14 +76,18 @@ return {
 
   {
     "windwp/nvim-ts-autotag",
-    opts = {
-      enable_close = true,          -- Auto close tags
-      enable_rename = true,         -- Auto rename pairs of tags
-      enable_close_on_slash = true, -- Auto close on trailing </
-    },
-    aliases = {
-      ["cshtml"] = "html",
-    },
+    config = function()
+      require("nvim-ts-autotag").setup({
+        opts = {
+          enable_close = true,          -- Auto close tags
+          enable_rename = true,         -- Auto rename pairs of tags
+          enable_close_on_slash = true, -- Auto close on trailing </
+        },
+        aliases = {
+          ["cshtml"] = "html",
+        },
+      })
+    end,
   },
 
   {
