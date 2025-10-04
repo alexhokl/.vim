07702ada4b56cada673d641b8ae0168f10a578b5
alexhokl <alexhokl@gmail.com>
2025-10-04 21:06:45 +0800

    Reverted to use the latest version of vim-dadbod from Tim Pope

--- lua/plugin/db.lua
+++ lua/plugin/db.lua
@@ -3,7 +3,7 @@ return {
   {
     "kristijanhusak/vim-dadbod-ui",
     dependencies = {
-      { 'alexhokl/vim-dadbod',                  lazy = true },
+      { 'tpope/vim-dadbod',                     lazy = true },
       { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
     },
     keys = {
