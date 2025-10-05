7a7d143be47b5b96048467505b727b32e79df50b
alexhokl <alexhokl@gmail.com>
2025-10-05 13:55:10 +0800

    Fixed configuration of sqls

--- lua/plugin/db.lua
+++ lua/plugin/db.lua
@@ -34,6 +34,7 @@ return {
 
   {
     "nanotee/sqls.nvim",
+    ft = { "sql", "mysql", "plsql" },
     opts = {},
     keys = {
       { "<leader>ds",   "<cmd>SqlsSwitchConnection<CR>",    mode = "n", desc = "Switch database connection (sqls)",    noremap = true, silent = true },
