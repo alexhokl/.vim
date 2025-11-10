40681b15843d1cec59276b3e1008c029feff68c4
alexhokl <alexhokl@gmail.com>
2025-11-10 20:07:12 +0900

    Updated configuration of plugin flutter-tools

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -21,12 +21,13 @@ return {
   },
 
   {
-    "akinsho/flutter-tools.nvim",
-    opts = {
-      debugger = {
-        enabled = true
-      },
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
     },
+    config = true,
     ft = { "dart" },
   },
 
