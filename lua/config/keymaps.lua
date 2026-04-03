9177351927190161a3b18198e3574e3ae33bee17
alexhokl <alexhokl@gmail.com>
2026-04-03 11:11:31 +0900

    Added keymap for coping the absolute path to the current buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 
 -- Utility function for mapping keys
 local map = function(mode, keys, command, desc)
-  vim.keymap.set(mode, keys, command, { noremap = true, silent = true, desc = desc })
+	vim.keymap.set(mode, keys, command, { noremap = true, silent = true, desc = desc })
 end
 
 -- Leader shortcuts (`,` is leader as set above)
@@ -10,7 +10,7 @@ end
 
 -- Normal mode mappings
 -- map("n", "<Space>", "zz", "Center screen on cursor") -- Press Space to center the screen at the cursor line
-map("n", "<C-x>", ":bnext<CR>", "Next buffer")         -- Ctrl-X: switch to next buffer
+map("n", "<C-x>", ":bnext<CR>", "Next buffer") -- Ctrl-X: switch to next buffer
 map("n", "<C-z>", ":bprevious<CR>", "Previous buffer") -- Ctrl-Z: switch to previous buffer (repurposed, was suspend)
 
 -- Multiple cursors (vim-visual-multi) will use <C-n> by default:
@@ -21,11 +21,11 @@ map("n", "<C-z>", ":bprevious<CR>", "Previous buffer") -- Ctrl-Z: switch to prev
 -- (Defined in plugin config via lazy keys, see plugins.lua)
 
 -- Other convenient mappings
-map("n", "Y", "y$", "Yank to end of line (like D/C)")      -- Make Y act like D (delete) and C (change)
-map("n", "n", "nzzzv", "Next search result centered")      -- After search, center next match
-map("n", "N", "Nzzzv", "Previous search result centered")  -- Center the screen on previous match
+map("n", "Y", "y$", "Yank to end of line (like D/C)") -- Make Y act like D (delete) and C (change)
+map("n", "n", "nzzzv", "Next search result centered") -- After search, center next match
+map("n", "N", "Nzzzv", "Previous search result centered") -- Center the screen on previous match
 map("n", "J", "mzJ`z", "Join lines without moving cursor") -- Join lines and keep cursor in place
-map("i", "jk", "<Esc>", "Exit insert mode quickly")        -- Press 'jk' fast in insert mode to exit to normal
+map("i", "jk", "<Esc>", "Exit insert mode quickly") -- Press 'jk' fast in insert mode to exit to normal
 
 -- Save and quit shortcuts
 map("n", "<Leader>w", ":w<CR>", "Save file")
@@ -46,7 +46,7 @@ map("n", "Q", "<NOP>", "no-op")
 
 -- hide highlighted search results
 map("n", "<leader><space>", function()
-  vim.cmd("nohlsearch")
+	vim.cmd("nohlsearch")
 end, "Hide search highlights")
 
 -- switch window
@@ -71,31 +71,45 @@ map("n", "#", ":let @/='\\v<' . expand('<cword>') . '>'<CR>:let v:searchforward=
 
 -- identation of a block
 map("v", "<", function()
-  vim.cmd("normal! <")
-  vim.cmd("normal! gv")
+	vim.cmd("normal! <")
+	vim.cmd("normal! gv")
 end, "Decrease indentation of selected block")
 map("v", ">", function()
-  vim.cmd("normal! >")
-  vim.cmd("normal! gv")
+	vim.cmd("normal! >")
+	vim.cmd("normal! gv")
 end, "Increase indentation of selected block")
 
 -- folding
-if vim.fn.has('folding') == 1 then
-  map("n", "<leader>c0", function() vim.o.foldlevel = 0 end, "Collapses to level 1")
-  map("n", "<leader>c1", function() vim.o.foldlevel = 1 end, "Collapses to level 2")
-  map("n", "<leader>c2", function() vim.o.foldlevel = 2 end, "Collapses to level 3")
-  map("n", "<leader>c3", function() vim.o.foldlevel = 3 end, "Collapses to level 4")
-  map("n", "<leader>c4", function() vim.o.foldlevel = 4 end, "Collapses to level 5")
-  map("n", "<leader>c5", function() vim.o.foldlevel = 5 end, "Collapses to level 6")
-  map("n", "<leader>c9", function() vim.o.foldlevel = 99 end, "Expand all folds")
+if vim.fn.has("folding") == 1 then
+	map("n", "<leader>c0", function()
+		vim.o.foldlevel = 0
+	end, "Collapses to level 1")
+	map("n", "<leader>c1", function()
+		vim.o.foldlevel = 1
+	end, "Collapses to level 2")
+	map("n", "<leader>c2", function()
+		vim.o.foldlevel = 2
+	end, "Collapses to level 3")
+	map("n", "<leader>c3", function()
+		vim.o.foldlevel = 3
+	end, "Collapses to level 4")
+	map("n", "<leader>c4", function()
+		vim.o.foldlevel = 4
+	end, "Collapses to level 5")
+	map("n", "<leader>c5", function()
+		vim.o.foldlevel = 5
+	end, "Collapses to level 6")
+	map("n", "<leader>c9", function()
+		vim.o.foldlevel = 99
+	end, "Expand all folds")
 end
 
 -- toggle relative number
 local toggle_relative_number = function()
-  -- get() is required as relativenumber is a table
-  vim.opt.relativenumber = not vim.opt.relativenumber:get()
+	-- get() is required as relativenumber is a table
+	vim.opt.relativenumber = not vim.opt.relativenumber:get()
 end
-map('n', '<leader>rn', toggle_relative_number, "Toggle relative number")
+map("n", "<leader>rn", toggle_relative_number, "Toggle relative number")
 
 -- show diagnostics in qickfix list
 map("n", "<leader>tdq", vim.diagnostic.setqflist, "Show diagnostics in quickfix list")
@@ -103,3 +117,11 @@ map("n", "<leader>tdq", vim.diagnostic.setqflist, "Show diagnostics in quickfix 
 -- avoid un-necessary modifications of registers
 map("n", "x", '"_x', "Delete character without yanking")
 map("n", "c", '"_c', "Change without yanking")
+
+-- copy absolute path to clipboard
+local copy_file_path = function()
+	local path = vim.fn.expand("%:p")
+	vim.fn.setreg("+", path)
+	print("Copied path: " .. path)
+end
+map("n", "<leader>fp", copy_file_path, "Copy current file path")
